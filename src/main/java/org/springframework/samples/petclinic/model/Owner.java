05ee092f0cf82af9ea35f4d1c06bf7e45e2c525f
Fran <fjbelrab@gmail.com>
2021-05-22 13:28:20 +0200

    TAREA 98: REQUEST CAMPO EMAIL
    Añade para owner el campo email

--- src/main/java/org/springframework/samples/petclinic/model/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/model/Owner.java
@@ -59,7 +59,12 @@ public class Owner extends Person {
 	@NotEmpty
 	@Digits(fraction = 0, integer = 10)
 	private String telephone;
+	
+	@Column(name = "email")
+	@NotEmpty
+	private String email;
 
+	
 	@OneToMany(cascade = CascadeType.REFRESH, mappedBy = "owner")
 	private Set<Pet> pets;
 	
@@ -99,6 +104,14 @@ public class Owner extends Person {
 
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	protected Set<Pet> getPetsInternal() {
